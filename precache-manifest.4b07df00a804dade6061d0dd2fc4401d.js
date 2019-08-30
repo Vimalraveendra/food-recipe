a552e432dbf6673d755e032d871225d197128246
vimal Raveendran <vimalraveendra@gmail.com>
2019-08-30 09:30:03 +0200

    Updates

---
+++ precache-manifest.4b07df00a804dade6061d0dd2fc4401d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256d18f80b6548cef11c6f9bb420bd19",
+    "url": "/food-recipe/index.html"
+  },
+  {
+    "revision": "cb812dee9c6dd28c6de9",
+    "url": "/food-recipe/static/css/main.5d09424e.chunk.css"
+  },
+  {
+    "revision": "563a6dac960d0f76bc99",
+    "url": "/food-recipe/static/js/2.d2cc4290.chunk.js"
+  },
+  {
+    "revision": "cb812dee9c6dd28c6de9",
+    "url": "/food-recipe/static/js/main.fab94545.chunk.js"
+  },
+  {
+    "revision": "7ff7a1efe2b3ff6e07c0",
+    "url": "/food-recipe/static/js/runtime~main.f99d16f5.js"
+  },
+  {
+    "revision": "7882912c5bec37217bdf05d0e298e0da",
+    "url": "/food-recipe/static/media/citonl.7882912c.woff"
+  }
+]);
