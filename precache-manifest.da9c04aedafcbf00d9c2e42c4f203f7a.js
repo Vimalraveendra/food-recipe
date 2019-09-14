6323603cdcb68ef9fa8151c5a4820dc7c36eef7d
vimal Raveendran <vimalraveendra@gmail.com>
2019-09-14 20:28:28 +0200

    Updates

--- precache-manifest.da9c04aedafcbf00d9c2e42c4f203f7a.js
+++ precache-manifest.da9c04aedafcbf00d9c2e42c4f203f7a.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "60ec9bd240ac7cfed9952db09a6a0f7d",
+    "revision": "45378bef96f9c11feab25a8edb6a3f35",
     "url": "/food-recipe/index.html"
   },
   {
-    "revision": "cd9c8f4562732322c169",
+    "revision": "aadd1be37176946e3cf2",
     "url": "/food-recipe/static/css/main.6cd1b0c6.chunk.css"
   },
   {
-    "revision": "563a6dac960d0f76bc99",
-    "url": "/food-recipe/static/js/2.d2cc4290.chunk.js"
+    "revision": "be0c3837409406aa3a3b",
+    "url": "/food-recipe/static/js/2.a0860713.chunk.js"
   },
   {
-    "revision": "cd9c8f4562732322c169",
-    "url": "/food-recipe/static/js/main.f15f898a.chunk.js"
+    "revision": "aadd1be37176946e3cf2",
+    "url": "/food-recipe/static/js/main.576085c7.chunk.js"
   },
   {
     "revision": "7ff7a1efe2b3ff6e07c0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/food-recipe/precache-manifest.8c945b9851d05a474d2683dadcec2d63.js"
+  "/food-recipe/precache-manifest.da9c04aedafcbf00d9c2e42c4f203f7a.js"
 );
 
 self.addEventListener('message', (event) => {
