dc4b00f094282c87f214ca00e8a33bf92cffe70f
vimal Raveendran <vimalraveendra@gmail.com>
2019-09-14 20:27:32 +0200

    updated Router component

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,18 @@
 import React from 'react';
 
-import {BrowserRouter,Route ,Switch} from 'react-router-dom';
+import {HashRouter,Route ,Switch} from 'react-router-dom';
 import App from './App';
 import RecipeDetails from './RecipeDetails';
 
 const Router = () =>{
-  console.log(process.env.PUBLIC_URL);
 	return(
         <React.Fragment>
-         <BrowserRouter >
+         <HashRouter >
            <Switch>
            <Route exact path = '/' component={App}/>
            <Route exact path ='/recipedetails/:id' component={RecipeDetails} />
            </Switch>
-         </BrowserRouter>
+         </HashRouter>
 
         </React.Fragment>
 
