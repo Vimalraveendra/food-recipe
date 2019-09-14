6bca757d8a043764be34a30ff9c0748d8a2db5cc
vimal Raveendran <vimalraveendra@gmail.com>
2019-09-14 20:40:00 +0200

    updated Router & Index

--- src/Router.js
+++ src/Router.js
@@ -1,19 +1,16 @@
 import React from 'react';
 
-import {HashRouter,Route ,Switch} from 'react-router-dom';
+import {Route ,Switch} from 'react-router-dom';
 import App from './App';
 import RecipeDetails from './RecipeDetails';
 
 const Router = () =>{
 	return(
         <React.Fragment>
-         <HashRouter >
            <Switch>
            <Route exact path = '/' component={App}/>
            <Route exact path ='/recipedetails/:id' component={RecipeDetails} />
            </Switch>
-         </HashRouter>
-
         </React.Fragment>
 
 		)
